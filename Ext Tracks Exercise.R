af5904379f1f6c0ff2cf8a9abb5d80df8419fbee
mtsy34 <mtsy34@gmail.com>
2017-10-27 00:04:26 +0800

    Updated, working data.

--- Ext Tracks Exercise.R
+++ Ext Tracks Exercise.R
@@ -25,5 +25,3 @@ ext_tracks_colnames <- c("storm_id", "storm_name", "month", "day",
 ext_tracks <- read_fwf(ext_tracks_file,  fwf_widths(ext_tracks_widths, ext_tracks_colnames), na = "-99")
 
 write.csv(ext_tracks, "./data/data.csv")
-
-rm(ext_tracks_colnames, ext_tracks_file, ext_tracks_widths, ext_tracks)
